34378cfb6739658b5475f18f9032e1b54853dc2f
albernucom <albertnu.mail@gmail.com>
2021-06-13 20:53:00 -0500

    Terminar proyecto base

---
+++ script.js
@@ -0,0 +1,57 @@
+//---------------------------------------------////
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+let coord = {x:0, y:0 };
+let drawing = false;
+let pointer = document.getElementById('pointer');
+let color = document.getElementById('color');
+let eraser = document.getElementById('eraser');
+
+
+canvas.addEventListener("mousedown", (e) => {
+    x = e.offsetX;
+    y= e.offsetY;
+    drawing = true;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    if (drawing === true) {
+        drawLine(ctx, x, y, e.offsetX, e.offsetY);
+        x = e.offsetX;
+        y = e.offsetY;
+    }
+});
+
+window.addEventListener("mouseup", (e) =>{
+    if (drawing === true){
+        drawLine(ctx, x, y, e.offsetX, e.offsetY);
+        x = 0;
+        y = 0;
+        drawing = false;
+    }
+});
+
+function drawLine(ctx, x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.lineWidth = pointer.value;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = color.value;
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+    ctx.closePath();
+}
+
+eraser.addEventListener("click", borrar, false);
+canvas.addEventListener("resize", resize);
+
+function resize() {
+    ctx.canvas.width = canvas.width;
+    ctx.canvas.height = canvas.height;
+}
+resize();
+
+function borrar()
+{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
